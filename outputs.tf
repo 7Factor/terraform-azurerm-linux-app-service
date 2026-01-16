9ececc0f61b3b9f736d497ce8d4e9dab597551c0
Greg Willard <gwillard@r3pwn.com>
2026-01-16 13:25:59 -0500

    consolidate outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,11 @@
-output "app_id" {
-  value = azurerm_linux_web_app.web_app.id
+output "web_app" {
+  value = azurerm_linux_web_app.web_app
 }
 
-output "app_default_hostname" {
-  value = azurerm_linux_web_app.web_app.default_hostname
+output "service_plan" {
+  value = azurerm_service_plan.web_app
 }
 
-output "app_service_plan_id" {
-  value = local.service_plan_id
-}
-
-output "application_insights_connection_string" {
-  value = azurerm_application_insights.web_app.connection_string
-}
-
-output "application_insights_id" {
-  value = azurerm_application_insights.web_app.id
+output "application_insights" {
+  value = azurerm_application_insights.web_app
 }
