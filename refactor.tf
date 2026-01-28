300d8afaa87247e8087303f066ad9731c1b56831
Greg Willard <gwillard@r3pwn.com>
2026-01-28 14:46:38 -0500

    fix refactor module

--- refactor.tf
+++ refactor.tf
@@ -1,6 +1,11 @@
 moved {
-  from = azurerm_application_insights.portal1
+  from = azurerm_key_vault.web_app[0]
   to   = module.app_secrets.azurerm_key_vault.vault[0]
+}
+
+moved {
+  from = azurerm_role_assignment.webapp_kv_reader[0]
+  to   = module.app_secrets.azurerm_role_assignment.webapp_kv_reader[0]
 }
 
 moved {
